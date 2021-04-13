5e4c119546c31f251e1d7f9d268db491fd016328
tangyan <ty@hcttop.com>
2021-04-13 16:11:11 +0800

    Initial commit

---
+++ src/test/java/com/ty/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
