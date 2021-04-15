17c4b39a77fb74da8ea03b718fdf69e282587423
tangyan <ty@hcttop.com>
2021-04-15 15:50:32 +0800

    拦截器

---
+++ src/main/java/com/ty/demo/config/LogInterceptor.java
@@ -0,0 +1,30 @@
+package com.ty.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("拦截器preHandle------------------");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("拦截器postHandle------------------");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("拦截器afterCompletion------------------");
+    }
+}
---
+++ src/main/java/com/ty/demo/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.ty.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // 注册一个安全拦截器，拦截所有请求（登录接口及 knife4j 的UI界面接口除外）
+    String[] knife4jPaths = new String[]{
+            "/doc.html",
+            "/webjars/**",
+            "/swagger-resources/**",
+            "/swagger-ui.html/**",
+            "/v2/api-docs",
+            "/favicon.ico",
+            "/error"
+    };
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())//添加拦截器
+                .addPathPatterns("/**")//拦截路径
+                .excludePathPatterns(knife4jPaths);//放行路径
+    }
+}
