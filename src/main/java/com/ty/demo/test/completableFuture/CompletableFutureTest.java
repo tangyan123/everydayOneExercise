918d35e72ad90dbb7e475eafa68cd8d28285b8d8
tangyan <ty@hcttop.com>
2021-12-29 15:36:37 +0800

    添加异步回调

--- src/main/java/com/ty/demo/test/completableFuture/CompletableFutureTest.java
+++ src/main/java/com/ty/demo/test/completableFuture/CompletableFutureTest.java
@@ -1,10 +1,27 @@
 package com.ty.demo.test.completableFuture;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.apache.poi.ss.formula.functions.T;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * 使用CompletableFuture场景
+ * 执行比较耗时的操作时，尤其是那些依赖一个或多个远程服务的操作，使用异步任务可以改善程序的性能，加快程序的响应速度
+ * 使用CompletableFuture类，它提供了异常管理的机制，让你有机会抛出、管理异步任务执行种发生的异常
+ * 如果这些异步任务之间相互独立，或者他们之间的的某一些的结果是另一些的输入，你可以讲这些异步任务构造或合并成一个
+ * 小贴士 ：测试多线程的小伙伴请勿使用JUit单元测试，因为JUnit在主线程完成之后就会关闭JVM，有兴趣的小伙伴请自行百度
+ * @throws ExecutionException
+ * @throws InterruptedException
+ */
 public class CompletableFutureTest {
 
     void test1() throws ExecutionException, InterruptedException {
@@ -57,8 +74,7 @@ public class CompletableFutureTest {
         System.out.println(handle.get());
     }
 
-    void test3() throws ExecutionException, InterruptedException {
-
+    void test3() {
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
                 return  null;
         });
@@ -76,10 +92,137 @@ public class CompletableFutureTest {
         thenApply.thenAccept(r -> System.out.println(r));
     }
 
+    void test4() {
+        ExecutorService executors= Executors.newFixedThreadPool(5);
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> 123).thenApply(i -> i.toString());
+        System.out.println(future.join());
+        CompletableFuture<List<String>> completableFuture = CompletableFuture.supplyAsync(() -> {
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                list.add("型号" + i);
+            }
+            return list;
+        }, executors).thenCompose(l -> {
+            CompletableFuture<List<String>> listCompletableFuture = CompletableFuture.supplyAsync(() -> {
+                List<String> list = new ArrayList<>();
+                for (int i = 0; i < 5; i++) {
+                    list.add("颜色" + i);
+                }
+                List<String> stringList = Stream.of(l, list).flatMap(Collection::stream).collect(Collectors.toList());
+                return stringList;
+            }, executors);
+            return  listCompletableFuture;
+        });
+        completableFuture.join().stream().forEach(l -> System.out.println(l));
+
+    }
+    void test5(){
+        CompletableFuture<List<String>> painting = CompletableFuture.supplyAsync(() -> {
+            // 第一个任务获取美术课需要带的东西，返回一个list
+            List<String> stuff = new ArrayList<>();
+            stuff.add("画笔");
+            stuff.add("颜料");
+            return stuff;
+        });
+        CompletableFuture<List<String>> handWork = CompletableFuture.supplyAsync(() -> {
+            // 第二个任务获取劳技课需要带的东西，返回一个list
+            List<String> stuff = new ArrayList<>();
+            stuff.add("剪刀");
+            stuff.add("折纸");
+            return stuff;
+        });
+        CompletableFuture<List<String>> total = painting
+                // 传入handWork列表，然后得到两个CompletableFuture的参数Stuff1和2
+                .thenCombine(handWork, (stuff1, stuff2) -> {
+                    // 合并成新的list
+                    List<String> totalStuff = Stream.of(stuff1, stuff1)
+                            .flatMap(Collection::stream)
+                            .collect(Collectors.toList());
+                    return totalStuff;
+                });
+        total.join().stream().forEach(l -> System.out.println(l));
+    }
+    void test6() throws ExecutionException, InterruptedException {
+        List<CompletableFuture> list=new ArrayList<>();
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> 100);
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "100");
+        CompletableFuture<Double> future2 = CompletableFuture.supplyAsync(() -> 100D);
+        list.add(future);
+        list.add(future1);
+        list.add(future2);
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(list.toArray(new CompletableFuture[list.size()]));
+        CompletableFuture<List<Object>> future3 = voidCompletableFuture.thenApply(v -> list.stream(
+                ).map(futures -> futures.join()).
+                        collect(Collectors.toList())
+        );
+        System.out.println(future3.join());
+    }
+    void test7(){
+        List<CompletableFuture> list=new ArrayList<>();
+        CompletableFuture<Object> completableFuture = CompletableFuture.supplyAsync(() -> {
+            throw new NullPointerException();
+        });
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return  "100";
+        });
+        CompletableFuture<Double> future2 = CompletableFuture.supplyAsync(() -> 10D);
+        list.add(completableFuture);
+        list.add(future1);
+        list.add(future2);
+        CompletableFuture<Object> completableFuture1 = CompletableFuture.anyOf(list.toArray(new CompletableFuture[list.size()]));
+        CompletableFuture<List<Object>> future3 = completableFuture1.thenApply(v -> list.stream(
+                ).map(futures -> futures.join()).
+                        collect(Collectors.toList())
+        ).exceptionally(e ->{
+            e.printStackTrace();
+            return Collections.singletonList("异常数据");
+        });
+        System.out.println(future3.join());
+    }
+    void test8(){
+        long begin = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<CompletableFuture<Integer>> futureList = IntStream.range(1, 10).mapToObj(i -> {
+            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+                if (i == 5) {
+                    throw new NullPointerException();
+                }
+                try {
+                    TimeUnit.SECONDS.sleep(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(i);
+                return i;
+            }, executorService).exceptionally(e -> {
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+                System.out.println("空指针异常");
+                return 100;
+            });
+            return future;
+        }).collect(Collectors.toList());
+        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[]{})).join();
+        System.out.println("最终耗时" + (System.currentTimeMillis() - begin) + "毫秒");
+        executorService.shutdown();
+    }
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         CompletableFutureTest test=new CompletableFutureTest();
-        //test.test1();
-       // test.test2();
-        test.test3();
+        // test.test1();
+        // test.test2();
+        // test.test3();
+        // test.test4();
+        // test.test5();
+        // test.test6();
+        // test.test7();
+        test.test8();
     }
 }
